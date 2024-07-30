64ba07911d8a51f825316ec7d31941c06ce4b36a
Brian T. <bptiburcio@gmail.com>
2024-07-30 18:38:28 -0400

    Fixes build

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,69 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+    return {
+        mode: "development",
+        entry: {
+            main: "./src/js/index.js",
+            install: "./src/js/install.js",
+        },
+        output: {
+            filename: "[name].bundle.js",
+            path: path.resolve(__dirname, "dist"),
+        },
+        plugins: [
+            new WebpackPwaManifest({
+                name: "JATE",
+                short_name: "JATE",
+                description: "Keep track of what matters most",
+                background_color: "#ffffff",
+                crossorigin: "anonymous", //can be null, use-credentials or anonymous
+                icons: [
+                    {
+                        src: path.resolve("src/images/logo.png"),
+                        sizes: [500], // multiple sizes
+                        purpose: "maskable",
+                    },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                template: "./index.html",
+                title: "Webpack Plugin",
+            }),
+            new MiniCssExtractPlugin(),
+            new InjectManifest({
+                swSrc: "./src-sw.js",
+                swDest: "sw.js",
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: "asset/resource",
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                        },
+                    },
+                },
+            ],
+        },
+    };
 };
