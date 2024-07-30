5b70547d0f7c70409128c27702188081f4a8bce4
Brian T. <bptiburcio@gmail.com>
2024-07-30 18:55:01 -0400

    adds data to local database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,47 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+    openDB("jate", 1, {
+        upgrade(db) {
+            if (db.objectStoreNames.contains("jate")) {
+                console.log("jate database already exists");
+                return;
+            }
+            db.createObjectStore("jate", {
+                keyPath: "id",
+                autoIncrement: true,
+            });
+            console.log("jate database created");
+        },
+    });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (name, home_phone, cell_phone, email) => {
+    console.log("Post to the database");
+
+    // Open the database.
+    const contactDb = await openDB("contact", 1);
+
+    // Open a transaction on the database.
+    const tx = contactDb.transaction("contact", "readwrite");
+
+    // Open the object store.
+    const store = tx.objectStore("contact");
+
+    // Add the data to the object store.
+    const request = store.add({
+        name,
+        home_phone,
+        cell_phone,
+        email,
+    });
+
+    // Wait for the database to complete the transaction.
+    const result = await request;
+    console.log("Data saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => console.error("getDb not implemented");
 
 initdb();
